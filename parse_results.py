348087a878505780584bf302654b6499f14d0570
zkza <zekuza909@gmail.com>
2023-03-29 22:46:24 +0800

    update rankings

--- parse_results.py
+++ parse_results.py
@@ -123,7 +123,7 @@ def main():
     chart_df = pd.DataFrame()
     for _, row in all_df.iterrows():
         print(row.teams)
-        players = list(np.array(row['teams']).flat)
+        players = list(np.array(row['teams']).flatten())
         ratings = [(elo.mu - 2 * elo.sigma) * 4 for elo in \
                 itertools.chain.from_iterable(row['elos'])]
         #ratings = [elo.mu * 4 for elo in np.array(row['elos']).flat]
