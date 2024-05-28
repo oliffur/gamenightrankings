a945a5472bcde820c828b4a751ec9befc8f4d6b5
Oliver Huang <oliffur@gmail.com>
2024-05-28 23:19:41 +0800

    update rankings

--- parse_results.py
+++ parse_results.py
@@ -127,7 +127,7 @@ def main():
         if rating.wins + rating.losses < 10: infrequent_players.append(player)
 
     print(plt.style.available)
-    plt.style.use('ggplot')
+    plt.style.use('seaborn-v0_8')
     chart_df = pd.DataFrame()
     for _, row in all_df.iterrows():
         players = list(np.array(row['teams']).flat)
