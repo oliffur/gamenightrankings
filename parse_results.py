dc95e9613cbe133c5081b79745756b46ad9df43f
Oliver Huang <oliffur@gmail.com>
2024-05-28 23:21:41 +0800

    update rankings

--- parse_results.py
+++ parse_results.py
@@ -127,7 +127,7 @@ def main():
         if rating.wins + rating.losses < 10: infrequent_players.append(player)
 
     print(plt.style.available)
-    plt.style.use('fivethirtyeight')
+    plt.style.use('ggplot')
     chart_df = pd.DataFrame()
     for _, row in all_df.iterrows():
         players = list(np.array(row['teams']).flat)
