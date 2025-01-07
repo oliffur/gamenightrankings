9cbd34e6b002fd763b401888a3ca8c2fd597a279
Oliver Huang <oliffur@gmail.com>
2025-01-07 15:33:31 +0800

    update rankings

--- parse_results.py
+++ parse_results.py
@@ -18,7 +18,7 @@ LUCK_FACTORS = {
     "No Thanks": 10.0,
     "Shifty Eyed Spies": 10.0,
     "Cash n Guns": 10.0,
-    "Bang": 10.0,
+    "Bang": 1.0,
     "Coup": 10.0,
     "Masquerade": 10.0,
     "Camel Up": 10.0,
